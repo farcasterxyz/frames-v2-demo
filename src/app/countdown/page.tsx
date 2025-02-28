3a224220386219dc3bb6cf4b74b78ca55133dc5c
crabsinger <83605543+crabsinger@users.noreply.github.com>
2025-02-27 17:04:22 -0800

    try adding meta gen

--- src/app/countdown/page.tsx
+++ src/app/countdown/page.tsx
@@ -1,6 +1,40 @@
 "use client";
-
 import { useEffect, useState } from "react";
+
+import { Metadata } from "next";
+
+// const appUrl = process.env.NEXT_PUBLIC_URL;
+const appUrl = process.env.VERCEL_URL;
+
+const frame = {
+  version: "next",
+  imageUrl: `https://${appUrl}/opengraph-image`,
+  button: {
+    title: "Launch Frame",
+    action: {
+      type: "launch_frame",
+      name: "Farcaster Frames v2 Demo",
+      url: `https://${appUrl}`,
+      splashImageUrl: `https://${appUrl}/splash.png`,
+      splashBackgroundColor: "#f7f7f7",
+    },
+  },
+};
+
+export const revalidate = 300;
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "Farcaster Frames v2 Demo",
+    openGraph: {
+      title: "Farcaster Frames v2 Demo",
+      description: "A Farcaster Frames v2 demo app.",
+    },
+    other: {
+      "fc:frame": JSON.stringify(frame),
+    },
+  };
+}
 
 // const START_DATE = new Date("2025-02-13"); // 2 weeks before Feb 27
 const END_DATE = new Date("2025-04-09"); // 6 weeks from Feb 26
