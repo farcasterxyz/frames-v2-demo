a74cd72c8eb453392b13d06085b23eaea09f8aef
crabsinger <83605543+crabsinger@users.noreply.github.com>
2025-03-04 14:46:41 -0800

    fix title

--- src/app/countdown/page.tsx
+++ src/app/countdown/page.tsx
@@ -24,10 +24,10 @@ export const revalidate = 300;
 
 export async function generateMetadata(): Promise<Metadata> {
   return {
-    title: "Farcaster Frames v2 Demo",
+    title: "Countdown Frame",
     openGraph: {
-      title: "Farcaster Frames v2 Demo",
-      description: "A Farcaster Frames v2 demo app.",
+      title: "Countdown Frame",
+      description: "The countdown iykyk.",
     },
     other: {
       "fc:frame": JSON.stringify(frame),
